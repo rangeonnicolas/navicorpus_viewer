8b73478dd27a17866015c0307ed210de133b3247
Nicolas <nicapps23@gmail.com>
2016-06-19 00:25:42 +0200

    changed path in import.sql

--- sample_data/script_import.sql
+++ sample_data/script_import.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE navicorpus;
+
 CREATE TABLE docs(
   corpus character varying(100),
   title text,
@@ -10,6 +12,6 @@ CREATE TABLE europarl___add_docs(
   policy text,
   url_votewatch text);
 
-COPY docs (corpus,title,url,id_doc) FROM '/usr/local/app/navicorpus/viewer/sample_data/europarl.csv' DELIMITER ';' CSV HEADER ;
-COPY europarl___add_docs (id_doc,date,policy,url_votewatch) FROM '/usr/local/app/navicorpus/viewer/sample_data/europarl_additional_data.csv' DELIMITER ';' CSV HEADER ;
+COPY docs (corpus,title,url,id_doc) FROM '/usr/local/app/navicorpus/navicorpus_viewer/sample_data/europarl.csv' DELIMITER ';' CSV HEADER ;
+COPY europarl___add_docs (id_doc,date,policy,url_votewatch) FROM '/usr/local/app/navicorpus/navicorpus_viewer/sample_data/europarl_additional_data.csv' DELIMITER ';' CSV HEADER ;
 
